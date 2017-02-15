b7fbd47649c9c239ceaf5d3ceed68cab1ad0e23f
sampath kumar <s8saamba@stud.uni-saarland.de>
2017-02-15 02:26:29 +0100

    latest

--- src/main/java/parser/early/Test_Read.java
+++ src/main/java/parser/early/Test_Read.java
@@ -2,11 +2,16 @@ package parser.early;
 
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter; 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Reader;
+import java.io.Writer;
 import java.net.URISyntaxException;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import parser.early.run.ParseResult;
@@ -31,8 +36,12 @@ public class Test_Read {
 		st = new MorphAdornoSentenceTokenizer();
 		ParseResult run = null;
 		try {
+			
 			Token[] tokens = st.getTokens(url_input_file);
 			run = parser.parseSentence(url_input_file,tokens);
+		
+			
+			
 			//System.out.println(run.getDerivedTrees().size());
 			//System.out.println("Read Sentence: "+st.getSentence(tokens));
 			
@@ -53,27 +62,48 @@ public class Test_Read {
        
        try {
            parser = new JTIGParser("");
-    	String url_lexicon = "C:\\Users\\sampath\\Desktop\\Master-thesis\\JTIG-master\\bin\\english-conll-5000.xml";
+    	String url_lexicon = "D:\\latest_jtig\\english-conll-5000.xml";
     	JTIGParser.setProperty("grammar.lexicon.path", url_lexicon);
-   		String path = "C:\\Users\\sampath\\Desktop\\Master-thesis\\JTIG-master\\bin\\eng-conll-ROOT2.txt";
+   		String path = "D:\\latest_jtig\\test.txt";
    		parser.readLexicon();
+   		
    		FileReader fileread = new FileReader(path) ;
    		
     	String sentence;
 		BufferedReader br = new BufferedReader(fileread);
-		while((sentence = br.readLine() )!= null){
-			tr.testLexicon_234_pt2(sentence);	
+		String pathtoWrite = "D:\\latest_jtig\\stats\\result.txt";
+		FileWriter fw= new FileWriter(pathtoWrite, true);
+	    BufferedWriter bw= new BufferedWriter(fw);
+		//long time= System.currentTimeMillis();
+       //	long time2;
+		int sentencenumber = 0;
+		String line = br.readLine();
+		while(line != null){
+			long begin = System.currentTimeMillis();
+			sentencenumber = sentencenumber+1;
+			tr.testLexicon_234_pt2(line);
+			LocalTime end = LocalTime.now();
 			
+			
+			bw.write("Time to write sentence "+ Integer.toString(sentencenumber) + " is " +  new Long(System.currentTimeMillis() -begin).toString()+" ms." + "\n");
+			line = br.readLine();
 		}
+		bw.close();
+		fw.close();
 		br.close();
 		fileread.close();
+		System.exit(1);
+		
 	
 		
 		
-	} catch (Exception e) {
+	
+	}
+	catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
+     
 	}
 
 }
